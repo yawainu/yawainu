461a59fbe861025d1701e8ec3ea184479304225d
yawainu <yanu@yawainu.com>
2016-06-03 04:40:56 +0900

    create models

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :user_sessions
   resources :users
 
-  get 'login' => 'user_sessions#new', :as => :login
+  get 'login'   => 'user_sessions#new',     :as => :login
   post 'logout' => 'user_sessions#destroy', :as => :logout
 end
