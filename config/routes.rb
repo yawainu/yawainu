297b182fc4cd5eb4a0fd3095f9301042421651e2
yawainu <yanu@yawainu.com>
2016-06-03 06:14:41 +0900

    add other controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,26 @@
 Rails.application.routes.draw do
+  get 'works/index'
+
+  get 'works/new'
+
+  get 'works/edit'
+
+  get 'galleries/index'
+
+  get 'galleries/new'
+
+  get 'galleries/edit'
+
+  get 'displays/edit'
+
+  get 'covers/edit'
+
+  get 'categories/index'
+
+  get 'categories/edit'
+
+  get 'categories/new'
+
   root :to => 'users#index'
   resources :user_sessions
   resources :users
