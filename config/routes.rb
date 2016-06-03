dd9a048d06301574f512d01c29e5829f17eaea7e
yawainu <yanu@yawainu.com>
2016-06-04 01:05:47 +0900

    works i,n,e,c,u,d

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,15 @@ Rails.application.routes.draw do
   root :to => 'user_sessions#new'
 
   resources :user_sessions, only:[:new, :create, :destroy]
-  resources :users,         only:[:show, :new, :edit, :create, :update]
   resources :user_infos,    only:[:show, :edit, :update]
   resources :covers,        only:[:edit, :update]
   resources :displays,      only:[:edit, :update]
-  resources :works,         only:[:index, :new, :edit, :create, :update, :destroy]
 
-  resources :categories, only:[:index, :new, :edit, :create, :update, :destroy] do
-    resources :galleries, only:[:index, :new, :edit, :create, :update, :destroy]
+  resources :users, only:[:show, :new, :edit, :create, :update] do
+    resources :works,         only:[:index, :new, :edit, :create, :update, :destroy]
+    resources :categories, only:[:index, :new, :edit, :create, :update, :destroy] do
+      resources :galleries, only:[:index, :new, :edit, :create, :update, :destroy]
+    end
   end
 
   get 'login'   => 'user_sessions#new',     :as => :login
